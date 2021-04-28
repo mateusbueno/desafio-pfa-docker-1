02b8b674c1a51820481fddd4527809dca6184a75
Mateus Bueno <mateus.bueno@infracommerce.com.br>
2021-04-28 10:09:22 -0300

    ajustes finais desafio

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Comandos para fazer a aplicação funcionar:
 
 docker network create pfa-1-network
 
-docker run -d --network pfa-1-network --name db -v $(pwd)/mysql:/var/lib/mysql -e MYSQL_DATABASE=nodedb -e MYSQL_ROOT_PASSWORD=root --restart always mysql:5.7 --innodb-use-native-aio=0
+docker run -d --network pfa-1-network --name db -v $(pwd)/banco:/var/lib/mysql --restart always mateusob/mysql-pfa-1 --innodb-use-native-aio=0
 
-docker run -d --rm --network pfa-1-network --name app mateusob/node-pfa-1 bash -c "npm i && node index.js"
+docker run -d --rm --network pfa-1-network --name app mateusob/node-pfa-1 bash -c "node index.js"
 
 docker run -d --rm --network pfa-1-network --name nginx -p 8080:80 mateusob/nginx-node
--- node/index.js
+++ node/index.js
@@ -11,37 +11,6 @@ const config = {
 const mysql = require('mysql')
 const conn = mysql.createConnection(config)
 
-criaTabela = () => {
-    return new Promise((resolve, reject) => {
-        conn.query(`CREATE TABLE IF NOT EXISTS nodedb.course_modules(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL, PRIMARY KEY (id), UNIQUE INDEX (name)) DEFAULT CHARSET=utf8;`,  (error, elements) => {
-            if (error) {
-                return reject(error);
-            }
-            return resolve(elements);
-        });
-    });
-};
-
-populaDados = () => {
-    return new Promise((resolve, reject) => {
-        conn.query(`INSERT IGNORE INTO nodedb.course_modules(name) VALUES 
-        ('Docker'),
-        ('Fundamentos de Arquitetura de Software'),
-        ('Comunicação'),
-        ('RabbitMQ'),
-        ('Autenticação e Keykloak'),
-        ('Domain Driven Design e Arquitetura hexagonal'),
-        ('Arquitetura do projeto prático - Codeflix'),
-        ('Microsserviço: Catálogo de vídeos com Laravel (Back-end)'),
-        ('Microsserviço: Catálogo de vídeos com React (Front-end)');`,  (error, elements) => {
-            if (error) {
-                return reject(error);
-            }
-            return resolve(elements);
-        });
-    });
-};
-
 buscaRegistros = () => {
     return new Promise((resolve, reject) => {
         conn.query(`SELECT * FROM course_modules ORDER BY id`,  (error, elements) => {
@@ -55,12 +24,10 @@ buscaRegistros = () => {
 
 app.get('/', async(req, res) => {
     try {
-        await criaTabela();
-        await populaDados();
         const resultSelect = await buscaRegistros();
         let listaModulos = '';
         for (let i = 0; i < resultSelect.length ; i++) {
-            listaModulos+='<p><b>User:</b> ' + resultSelect[i].name + ' - <b>Id:</b> ' + resultSelect[i].id + '</p>';
+            listaModulos+='<p><b>User:</b> ' + utf8.decode(resultSelect[i].name) + ' - <b>Id:</b> ' + resultSelect[i].id + '</p>';
         }
         res.send('<h1>Lista de módulos do curso Full Cycle</h1>' + listaModulos)
     } catch(e) {
